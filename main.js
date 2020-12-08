077890528bde92de4a24d06e27cee137cd16b9b7
v0ss3n <m.vossen@live.nl>
2020-12-08 15:28:11 +0100

    Start project page

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $('.process-carousel').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    $('.color-carousel').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    $('.final-carousel').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
